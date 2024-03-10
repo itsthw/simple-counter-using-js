2a9d07514decdc3ec9afb213791f3318939ed1c5
ThevinduH <120010745+itsthw@users.noreply.github.com>
2024-03-10 20:13:18 +0530

    push ~ javascript content

--- counter/script.js
+++ counter/script.js
@@ -1,1 +1,18 @@
+const increase = document.getElementById("btn--increase");
+const reset = document.getElementById("btn--reset");
+const decrease = document.getElementById("btn--decrease");
+const counter = document.getElementById("counter");
+let count = 0;
 
+increase.onclick = function() {
+    count++;
+    counter.textContent = count;
+}
+decrease.onclick = function() {
+    count--;
+    counter.textContent = count;
+}
+reset.onclick = function() {
+    count = 0;
+    counter.textContent = count;
+}
